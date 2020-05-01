58787019f4617a8b6202d556c97701fef39cdba2
RpaDoodle8080 <wbh3516@gmail.com>
2020-05-01 13:54:53 +0800

    Initial commit

--- views/customer_home.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-customer_home_view = Blueprint('customer_home_routes', __name__)
-
-@customer_home_view.route('/', methods=['GET'])
-def home():
-    return render_template('<h1>Hello World</h1>')
