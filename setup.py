36d4edcd7a49736e5b7705b75f8b28e29c1faff4
RpaDoodle8080 <wbh3516@gmail.com>
2020-05-03 00:34:45 +0800

    requirements.txt and finish the DB desgin v1

--- setup.py
+++ setup.py
@@ -12,11 +12,14 @@ def setup():
     db_name = input() or 'mooli'
 
     try:
-        connection = pymysql.connect(db_url, db_username, db_password, db_name)
+        connection = pymysql.connect(db_url, db_username, db_password)
     except:
         print('Unable to connect to database.')
         exit()
 
+    connection.cursor().execute('DROP DATABASE IF EXISTS {}'.format(db_name))
+    connection.cursor().execute('CREATE DATABASE IF NOT EXISTS {}'.format(db_name))
+    connection.cursor().execute('use {}'.format(db_name))
     print('Connection with database established.')
     init_db_tables(connection)
     connection.close()
@@ -57,7 +60,7 @@ def init_db_tables(connection):
             PRIMARY KEY (name)
         )""",
         """CREATE TABLE IF NOT EXISTS product (
-            product_id INT UNSIGNED,
+            product_id INT UNSIGNED AUTO_INCREMENT,
             name VARCHAR(255) NOT NULL,
             description TEXT,
             category VARCHAR(32) NOT NULL,
@@ -68,6 +71,53 @@ def init_db_tables(connection):
             PRIMARY KEY (product_id),
             FOREIGN KEY (category) REFERENCES category(name)
             )
+        """,
+        """CREATE TABLE IF NOT EXISTS `order`(
+            order_id INT UNSIGNED AUTO_INCREMENT,
+            total DECIMAL(8, 2),
+            discount DECIMAL(8, 2),
+            actual_paid DECIMAL(8, 2),
+            status INT,
+            purchased_date TIMESTAMP,
+            PRIMARY KEY (order_id)
+            )
+        """,
+        """CREATE TABLE IF NOT EXISTS item (
+            item_id INT UNSIGNED AUTO_INCREMENT,
+            customer_id INT(8) UNSIGNED,
+            product_id INT UNSIGNED,
+            order_id INT UNSIGNED,
+            amount INT,
+            PRIMARY KEY (item_id),
+            FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+            FOREIGN KEY (product_id) REFERENCES product(product_id),
+            FOREIGN KEY (order_id) REFERENCES `order`(order_id)
+        )
+        """,
+        """CREATE TABLE IF NOT EXISTS coupon (
+            coupon_code VARCHAR(32),
+            amount DECIMAL(8, 2),
+            threshold DECIMAL(3, 2),
+            percentage DECIMAL (3, 2),
+            PRIMARY KEY (coupon_code)
+        )
+        """,
+        """CREATE TABLE IF NOT EXISTS redeem_card (
+            redeem_code CHAR(16),
+            amount DECIMAL(8, 2),
+            PRIMARY KEY (redeem_code)
+        )
+        """,
+        """CREATE TABLE IF NOT EXISTS comment (
+            comment_id INT AUTO_INCREMENT,
+            customer_id INT(8) UNSIGNED,
+            product_id INT UNSIGNED,
+            rating DECIMAL(2,1),
+            body VARCHAR(140),
+            PRIMARY KEY (comment_id),
+            FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+            FOREIGN KEY (product_id) REFERENCES product(product_id)
+        )
         """,
     ]
 
