053782b177d7214850e6c32e3b5eaacc96e4cfdf
RpaDoodle8080 <wbh3516@gmail.com>
2020-05-06 00:18:42 +0800

    Changes to directory to routers

--- views/customer_home.py
+++ views/customer_home.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template
 
-customer_home_view = Blueprint('customer_home_routes', __name__, template_folder='../templates/customer')
+customer_home_view = Blueprint('customer_home_routes', __name__, template_folder='/templates')
 
 @customer_home_view.route('/', methods=['GET'])
 def home():
-    return render_template('home.html')
+    return render_template('customer/home.html')
 
 @customer_home_view.route('/test/<string:template>', methods=['GET'])
 def test(template):
-    return render_template(template)
+    return render_template('customer/' + template)
