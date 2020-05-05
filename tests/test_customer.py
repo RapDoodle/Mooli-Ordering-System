df563f9ba9b7263d3d0f127e74921669238047c9
RpaDoodle8080 <wbh3516@gmail.com>
2020-05-05 16:29:15 +0800

    Add: model_customer

---
+++ tests/test_customer.py
@@ -0,0 +1,7 @@
+import models.model_customer as m
+
+print('[UNIT TESTING] Testing: Customer...')
+
+m.add_customer('bowenwu', 'bowenwu@gmail.com', 'Testpassword123', 'Bowen', 'WU', 'M', '(310)-873-7333')
+
+print('[UNIT TESTING] Customer has passed all the tests')
---
+++ utils/validation.py
@@ -0,0 +1,40 @@
+import re
+from validate_email import validate_email
+
+def verify_regex(regex, d):
+    d = str(d)
+    regex = re.compile(regex)
+    if regex.match(d) is None:
+        return False
+    return True
+
+def is_money(d):
+    return verify_regex('^[0-9]+(\.[0-9]{1,2})?$', d)
+
+def is_valid_email(d):
+    return validate_email(d)
+
+def is_valid_length(d, min, max):
+    # The function checks whether d's length is in the range [min, max]
+    d = str(d)
+    if (len(d) >= min and len(d) <= max):
+        return True
+    return False
+
+def is_valid_username(d):
+    # The current system is desgined to accept username in the range between 8 and 24
+    return verify_regex('^[a-zA-Z0-9_-]{6,24}$', d)
+
+def is_valid_password(d):
+    # The password should contain at least:
+    #  - One lower case letter
+    #  - One upper case latter
+    #  - One number
+    if is_valid_length(d, 8, 24):
+        return verify_regex('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])', d)
+    return False
+
+if __name__ == '__main__':
+    print(is_valid_password('Testpassword123'))
+    print(is_valid_username('1234567812345678123456789'))
+    print(is_valid_email('bowenwu@gmail.com'))
