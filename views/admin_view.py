dd73685df5b41cac6bfde60819b53c79c4be4410
RpaDoodle8080 <wbh3516@gmail.com>
2020-05-06 08:22:05 +0800

    Fix: renaming bug

--- views/admin_view.py
+++ views/admin_view.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template
 
-staff_view = Blueprint('admin_view', __name__, template_folder='/templates')
+admin_view = Blueprint('admin_view', __name__, template_folder='/templates')
 
-@staff_view.route('/admin/dashboard', methods=['GET'])
+@admin_view.route('/admin/dashboard', methods=['GET'])
 def dashboard():
     return render_template('/admin/dashboard.html')
 
-@staff_view.route('/admin/test/<string:template>', methods=['GET'])
+@admin_view.route('/admin/test/<string:template>', methods=['GET'])
 def test_view(template):
     return render_template('/admin/' + template)
