109920ee72c12d044e46c7654adbad36ff9bdafe
RpaDoodle8080 <wbh3516@gmail.com>
2020-05-07 10:42:16 +0800

    new setup and config system

--- app.py
+++ app.py
@@ -11,4 +11,11 @@ app.register_blueprint(customer_home_view)
 app.register_blueprint(admin_view)
 
 if __name__ == '__main__':
-	app.run(port = 8000, debug = True)
+	port = config.get('port')
+	debug = config.get('debug')
+	if config.get('enable_https'):
+		ssl_context = (config.get('cert_path'), config.get('private_key_path'))
+		print(ssl_context)
+		app.run(port = port, debug = debug, ssl_context = ssl_context)
+	else:
+		app.run(port = port, debug = debug)
