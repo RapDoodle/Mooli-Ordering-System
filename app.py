b89c017543304d3873dbb3827e9fcfd3fb5e48dd
RpaDoodle8080 <wbh3516@gmail.com>
2020-05-15 00:21:27 +0800

    auth, and manage products (half way finish)

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ if __name__ == '__main__':
 	debug = config.get('debug')
 	if config.get('enable_https'):
 		ssl_context = (config.get('cert_path'), config.get('private_key_path'))
-		app.run(port = port, debug = debug, ssl_context = ssl_context)
+		app.run(host = '0.0.0.0', port = port, debug = debug, ssl_context = ssl_context)
 	else:
 		app.run(host = '0.0.0.0', port = port, debug = debug)
