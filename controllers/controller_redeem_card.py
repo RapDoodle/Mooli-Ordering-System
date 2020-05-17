8a2001e599c8d1494cfe6872b18b10661ec91c05
RpaDoodle8080 <wbh3516@gmail.com>
2020-05-17 08:41:52 +0800

    local 2

---
+++ controllers/controller_redeem_card.py
@@ -0,0 +1,15 @@
+import models.model_redeem_card as m
+from utils.exception import excpetion_handler
+
+@excpetion_handler
+def add_redeem_cards(*args, **kwargs):
+    return m.add_redeem_cards(*args, **kwargs)
+
+@excpetion_handler
+def get_redeem_cards(page = 1):
+    # Each page contains 20 records
+    return m.get_redeem_cards(limit = 20, offset = (page - 1) * 20)
+
+@excpetion_handler
+def delete_redeem_card(*args, **kwargs):
+    return m.delete_redeem_card(*args, **kwargs)
