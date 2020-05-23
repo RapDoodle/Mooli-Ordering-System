7e1abe969cf0c033b977a8424b7c53806da0b8a7
RapDoodle8080 <wbh3516@gmail.com>
2020-05-23 20:56:00 +0800

    Revert "added criteria for return info"
    This reverts commit 26866b0e41159a7ad5853228482ed9df8275d372.

--- tests/test_cart_item.py
+++ tests/test_cart_item.py
@@ -8,7 +8,6 @@ m.create_cart_item(10000, 2, 3)
 m.update_cart_item_amount(2, 2)
 m.create_cart_item(10000, 3, 4)
 m.create_cart_item(10001, 1, 8)
-m.get_cart_items_by_user_id(10000)
-assert list(m.get_cart_items_by_user_id(10000)[0].keys()) == ['cart_item_id', 'product_id', 'product_name', 'price', 'amount']
+assert len(m.get_cart_items_by_user_id(10000)) == 2
 
 print(' ✓ Cart Item has passed all the tests')
