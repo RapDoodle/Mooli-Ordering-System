d1018aa0592b89ae918c2e753a521fab37de779a
RapDoodle8080 <wbh3516@gmail.com>
2020-05-23 20:56:00 +0800

    Revert "added criteria for return info"
    This reverts commit 26866b0e41159a7ad5853228482ed9df8275d372.

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -13,7 +13,6 @@ m.remove_comment(4)
 m.remove_comment(5)
 m.remove_comment(6)
 assert len(m.get_comments(param = 1, limit = 5, offset = 0)) == 3
-assert list(m.get_comments(param = 1, limit = 5, offset = 0)[0].keys()) == ['comment_id', 'user_id', 'product_id', 'rating', 'body', 'created_at']
 m.add_comment(10001, 2, 5, 'Love it!')
 assert str(m.get_product_ratings(1)) == '3.67'
 assert str(m.get_product_ratings(1)) == '3.67'
